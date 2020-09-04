931a1d4e787546700c5e1bfa66271abc85499c4b
mzq737 <60275847+mzq737@users.noreply.github.com>
2020-09-03 22:01:29 -0400

    Updates

---
+++ precache-manifest.5829531ddfc6f91574391455e3082fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79e30fd46109ff15bab3953aaee72ff",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e5e73be90864234b8810",
+    "url": "/monsters-rolodex/static/css/main.08c9dc9f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e73be90864234b8810",
+    "url": "/monsters-rolodex/static/js/main.d8725c33.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
